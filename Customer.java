0e77ebbfc2d7689078f5fca20015db2eb96a9e97
Ayşegül AKPINARLI <aysegul.akpinarli@ug.bilkent.edu.tr>
2023-12-24 22:44:57 +0300

    car id variable of customers is changed to arrayList

--- Customer.java
+++ Customer.java
@@ -3,13 +3,17 @@
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+//import OtherClasses.Wallet;
 
 public class Customer extends User implements Comparable<Customer> {
 
-	private double expense;
-	private int carId;
+	private ArrayList<Integer> carId = new ArrayList<>();
 	private String saleDate;
+	private Wallet wallet;
 	
 	public Customer() {
 		
@@ -17,45 +21,53 @@ public class Customer extends User implements Comparable<Customer> {
 
 	public Customer(int carId, double expense) {
 		
-		this.carId = carId;
+		this.carId.add(carId);
 		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
 		Date date = new Date();
 		this.saleDate = dateFormat.format(date);
-		this.expense = expense;
+		wallet.purchase(expense);
 	}
 
 	public Customer(int id, String nameSurname, String phoneNum, String address,
-					 String userType) {
+					 String userType, String password) {
 
-		super(id, nameSurname, phoneNum, address, userType);
+		super(id, nameSurname, phoneNum, address, userType, password);
+	}
+	
+	public Customer(int id, String nameSurname, String phoneNum, String address,
+			 String userType, String password, double walletMoney) {
+	
+		super(id, nameSurname, phoneNum, address, userType, password);
+		Wallet w = new Wallet(walletMoney, 0);
+		this.wallet = w;
 	}
 	
 	public Customer(int id, String nameSurname, String phoneNum, String address, 
-					String userType, int carId, double expense) {
+					String userType, int carId,Wallet wallet, String password) {
 
-		super(id, nameSurname, phoneNum, address, userType);
-		this.carId = carId;
+		super(id, nameSurname, phoneNum, address, userType, password);
+		this.carId.add(carId);
 		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
 		Date date = new Date();
 		this.saleDate = dateFormat.format(date);
-		this.expense = expense;
+		this.wallet = wallet;
 	}
 
 	public Customer(int id, String nameSurname, String phoneNum, String address, 
-					String userType, int carId, double expense, String date) {
+					String userType, int carId, Wallet wallet, String date, String password) {
 						
-		super(id, nameSurname, phoneNum, address, userType);
-		this.carId = carId;
+		super(id, nameSurname, phoneNum, address, userType, password);
+		this.carId.add(carId);
 		this.saleDate = date;
-		this.expense = expense;
+		this.wallet = wallet;
 	}
 
-	public int getCarId() {
+	public ArrayList<Integer> getCarId() {
 		return carId;
 	}
 
 	public void setCarId(int carId) {
-		this.carId = carId;
+		this.carId.add(carId);
 	}
 
 	public String getsaleDate() {
@@ -65,26 +77,34 @@ public class Customer extends User implements Comparable<Customer> {
 	public void setsaleDate(String saleDate) {
 		this.saleDate = saleDate;
 	}
-
-	public double getExpense() {
-		return expense;
-	}
-
-	public void addToExpense(double expense) {
-		this.expense += expense;
-	}
-
 	
+	
+	public Wallet getWallet() {
+		return wallet;
+	}
+
+	public void setWallet(Wallet wallet) {
+		this.wallet = wallet;
+	}
+	
+	public void addWallet(double m) {
+		this.wallet.addMoney(m);
+	}
+
 	@Override
 	public String toString() {
-		return super.toString() + "Customer's Car Id = " + carId + 
-				"\nSale Date = " + saleDate + 
-				"\nExpense = " + expense + "\n";
+		String res = "";
+		for(Integer i : carId) {
+			res += i.toString() + "\n\t\t";
+		}
+		return super.toString() + "Customer's Car Id Numbers = " + res + 
+				"Sale Date = " + saleDate + "\n" +
+				wallet.toString();
 	}
 
 	@Override
 	public int compareTo(Customer o) {
-		return (int) (o.getExpense() - expense);
+		return (int) (o.wallet.getExpense() - wallet.getExpense());
 	}
 
 	
